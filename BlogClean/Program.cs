8257c96ddbbb56aee28fd50fe0e603f51168ae7f
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-01-11 01:03:27 +0330

    Add Refrences And Config DataBase

--- BlogClean/Program.cs
+++ BlogClean/Program.cs
@@ -1,9 +1,29 @@
+using Infra.Data.Contex;
+using Infra.Ioc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+//Data Base
+#region Config DataBase
+
+builder.Services.AddDbContext<ApplicationDbContxt>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDB"));
+});
+#endregion
+//Ioc Dependency
+#region Dependency Injection IoC
+
+DependencyContainer.RegisterServices(builder.Services);
+
+#endregion
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ Infra.Data/Contex/ApplicationDbContxt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.Contex
+{
+    public class ApplicationDbContxt:DbContext
+    {
+        public ApplicationDbContxt(DbContextOptions<ApplicationDbContxt> options) : base(options)
+        {
+
+        }
+    }
+}
