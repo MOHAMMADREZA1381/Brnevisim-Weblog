065d483a2d6a19b617726833472bf03ec5b3e8ad
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-01-12 01:00:55 +0330

    Add Area For User

---
+++ BlogClean/Areas/UserPanel/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogClean.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- BlogClean/Program.cs
+++ BlogClean/Program.cs
@@ -40,12 +40,15 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-app.MapControllerRoute(
-    name: "areas",
-    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-);
+
+
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+);
 app.Run();
