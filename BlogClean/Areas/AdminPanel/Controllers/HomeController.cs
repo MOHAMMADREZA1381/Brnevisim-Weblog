b6d3305a6e3597984c6d48cb5e002ef744ff9184
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-01-14 23:40:05 +0330

    Add Area User

---
+++ BlogClean/Areas/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogClean.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class HomeController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
