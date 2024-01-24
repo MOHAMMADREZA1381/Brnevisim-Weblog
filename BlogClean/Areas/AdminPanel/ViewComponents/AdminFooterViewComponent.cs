6cb4b94838dd6c7958b9d6c2a5abd4f9e159824f
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-01-25 02:06:38 +0330

    Add AdminPanel Views

---
+++ BlogClean/Areas/AdminPanel/ViewComponents/AdminFooterViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogClean.Areas.AdminPanel.ViewComponents
+{
+    public class AdminFooterViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("AdminFooter");
+        }
+    }
+}
---
+++ BlogClean/Areas/AdminPanel/ViewComponents/AdminHeaderViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogClean.Areas.AdminPanel.ViewComponents
+{
+    public class AdminHeaderViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View("AdminHeader");
+        }
+    }
+}
