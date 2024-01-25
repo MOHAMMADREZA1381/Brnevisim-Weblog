a7899d9a15e57981f4fdba92eec213e548b84cd2
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-01-26 00:29:09 +0330

    Add UserList View For Admin

---
+++ Application/ImageTools/PathExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.ImageTools
+{
+    public static class PathExtensions
+    {
+        #region BodyBlogImages
+
+        public static string UserAvatarOrgin= "/images/blog/origin/";
+        public static string UserAvatarOrginServer=Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/userAvatar/origin/");
+
+        public static string UserAvatarThumb = "/images/blog/thumb/";
+        public static string UserAvatarThumbServer = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/userAvatar/thumb/");
+
+        #endregion
+
+       
+    }
+}
