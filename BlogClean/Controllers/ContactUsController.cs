4fc2455b486a58ef1f9c1668de279bb77738a993
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-02-17 21:36:17 +0330

    add sweet alert and add some edit on contact us action

--- BlogClean/Controllers/ContactUsController.cs
+++ BlogClean/Controllers/ContactUsController.cs
@@ -1,26 +1,42 @@
-﻿using Domain.ViewModels.ContactUs;
+﻿using Application.Interfaces;
+using Domain.ViewModels.ContactUs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogClean.Controllers
 {
     public class ContactUsController : Controller
     {
+        #region Service
 
-
-        public IActionResult Index()
+        private readonly IContactUsService _contactUsService;
+        public ContactUsController(IContactUsService contactUsService)
         {
-            return View();
+            _contactUsService = contactUsService;
         }
+        #endregion
+
 
         [HttpGet("Contact-Us")]
-        public async Task<IActionResult> ContactUsRequest()
+        public async Task<IActionResult> Index()
         {
+     
             return View();
         }
 
         [HttpPost("Contact-Us")]
-        public async Task<IActionResult> ContactUsRequest(ContactUsViewModel model)
+        public async Task<IActionResult> Index(ContactUsViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                await _contactUsService.AddContactUs(model);
+                ModelState.Clear();
+                TempData["MessageType"] = "Success";
+            }
+            else
+            {
+              TempData["MessageType"] = "Error";
+            }
+
             return View();
         }
 
