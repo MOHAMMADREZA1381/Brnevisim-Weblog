0efcb689d7ac7222a042df3ca2b886fa455d4dd0
MOHAMMADREZA1381 <mohammadrza1381@gmail.com>
2024-08-19 19:32:17 +0330

    Update-Changes

---
+++ BlogClean/wwwroot/js/yektanetCustom.js
@@ -0,0 +1,26 @@
+﻿function load() {
+
+    var banner = document.getElementById("pos-article-text-card-95690");
+    banner.click();
+    console.log(banner.innerHTML);
+    var testlink = banner.innerHTML;
+
+
+    var shit = document.querySelector("a.yn-item-link");
+    console.log(shit);
+    var link = shit.getAttribute('href');
+    console.log(link);
+   /* window.open(link);*/
+
+
+  //document.querySelectorAll("div#pos-article-text-card-95690");
+  //  //var main = test.innerHTML;
+  //  //console.log(main);
+  //  for (var i = 0; i < shit.length; i++) {
+  //      shit[i].click();
+  //  }
+
+    
+
+};
+
